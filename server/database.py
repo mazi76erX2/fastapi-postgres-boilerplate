1b79a039dc358e058dbdad9a964eb666b2c63de7
Xolani <mazi76erx@gmail.com>
2025-03-07 06:17:22 +0000

    Updated imports

--- server/database.py
+++ server/database.py
@@ -4,7 +4,7 @@ from typing import AsyncGenerator
 
 from config import DATABASE_URL
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 engine: AsyncEngine = create_async_engine(DATABASE_URL, echo=True)
 async_session: sessionmaker = sessionmaker(
